ad37659e25580d8750a6217f387d2a6242c83c6c
Ana <avalladaresgonzalez@danielcastelao.org>
2024-02-28 10:48:04 +0100

    Boletin19_

---
+++ 2Trimestre/Boletines/Boletin19_/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Canario c = new Canario();
+        c.cantar();
+        Persoa p = new Persoa();
+        p.cantar();
+    }
+}
---
+++ 2Trimestre/Boletines/Boletin19_/src/Persoa.java
@@ -0,0 +1,5 @@
+public class Persoa implements IPodeCantar {
+    public void cantar() {
+        System.out.println("Do, re, mi, fa, sol, la, si, do!");
+    }
+}
